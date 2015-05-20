2df37a3874dec5344271211bd64fff1a53488d1d
Steven Iseki Martin <stevenisekimartin@gmail.com>
2015-05-20 17:41:35 +1000

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,22 @@
 
+'use strict';
+
+module.exports = function(app) {
+
+	/* home route */
+	var HomeRoute = require('./home');
+	app.get('/', HomeRoute);
+
+	var ExtractRoute = require('./extract');
+	app.get('/products/:productId/extract/:extractId', ExtractRoute.view);
+
+	/* Browse Categories */	
+	var BrowseRoute = require('./browse');
+	app.get('/browse/:category', BrowseRoute.view);
+
+
+	/* 404 */
+	var NotFoundRoute = require('./not-found');
+	app.get('*', NotFoundRoute);
+
+};
